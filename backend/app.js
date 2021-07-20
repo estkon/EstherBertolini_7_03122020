4086e6d8d52e7267519dfee30806baa9f9103da3
Esther Bertolini <esther.kondratieff@gmail.com>
2021-07-20 18:19:04 +0200

    backend

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const routerUser = require('./routes/user');
+require('dotenv').config()
+
+const app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express());
+app.use('images',express.static(path.join(__dirname, '/images')));
+app.use('/api/user', routerUser);
+
+module.exports = app;
+
---
+++ backend/models/post.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class post extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      models.post.belongsTo(models.user)// le modèle post peut avoir plusieur user
+      models.post.hasMany(models.like) // le modèle post peut avoir plusieur like
+    }
+  };
+  post.init({
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    image: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'post',
+  });
+  return post;
+};
