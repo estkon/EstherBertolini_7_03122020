69aeb82a97c7d7b39d8486e01dbce43666320fbc
Esther Bertolini <esther.kondratieff@gmail.com>
2021-08-05 09:38:03 +0200

    indentation

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,55 +1,48 @@
 <template>
-<div class="postList">
-
-        <div  v-if= "posts">
-            <div v-for = "post in posts" :key= "post.id">
-                <PostCard :post="post"/>
-            </div>
-        </div>
-
-</div>
-
+  <div class="postList">
+    <div v-if="posts">
+      <div v-for="post in posts" :key="post.id">
+        <PostCard :post="post" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import axios from "axios"
-import {mapGetters} from 'vuex'
-import PostCard from '../components/PostCard.vue'
+import axios from "axios";
+import { mapGetters } from "vuex";
+import PostCard from "../components/PostCard.vue";
 
 export default {
-    name:'Home',
-    components:{
+  name: "Home",
+  components: {
+    PostCard,
+  },
+  computed: {
+    ...mapGetters(["user"]),
+  },
 
-      PostCard,
-      
-    },
-    computed: {
-        ...mapGetters(['user'])
-    },
-     
-     data(){
-         return {
-             posts: null,
-         }
-     },
-    //récupération des posts depuis la BDD via axios
-     created(){
-         axios.get('http://localhost:8000/api/post') 
-            .then( response => {
-                console.log(response)
-                this.posts = response.data
-
-            })
-            .catch( error => console.log(error));
-     }
+  data() {
+    return {
+      posts: null,
+    };
+  },
+  //récupération des posts depuis la BDD via axios
+  created() {
+    axios
+      .get("http://localhost:8000/api/post")
+      .then((response) => {
+        console.log(response);
+        this.posts = response.data;
+      })
+      .catch((error) => console.log(error));
+  },
 };
-
 </script>
 
 <style scoped>
-.postList{
-        width: 80%;
-        margin:auto;
+.postList {
+  width: 80%;
+  margin: auto;
 }
-
 </style>
